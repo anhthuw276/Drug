406236856052481001b119973891fa078c2faaae
Kimnhun <nguyennhung99762@gmail.com>
2024-12-10 22:09:14 +0700

    Update MENU.py

--- MENU.py
+++ MENU.py
@@ -26,6 +26,25 @@ def Tao_du_lieu_moi() :
     df = pd.read_excel("d:/Drug (4).xlsx",engine="openpyxl")
     # In kết quả tạo dữ liệu mới
     print(df)
+def Tao_data() :
+    # Khai báo thư viện
+    import pandas as pd
+    # mở + đọc file 
+    file_path = 'Drug_cleanedcc.xlsx'  #bạn đặt tên file ntn thì sửa lại như vậy để code tìm 
+    data = pd.read_excel("d:/Drug (1).xlsx",engine='openpyxl')
+    # lọc / tìm kiếm thông tin chứa :"Acute Bacterial Sinusitis"
+    condition_to_cut = "Acute Bacterial Sinusitis" # điều kiện
+    filtered_rows = data[data['Condition'].str.contains(condition_to_cut, na=False)] # phần tìm kiếm thông tin
+    # dữ liệu từ file cũ (tức là các dữ liệu ko liên quan sẽ được giữ nguyên)
+    remaining_data = data[data['Condition'].str.contains(condition_to_cut, na=False)]
+    # lưu dữ liệu các hàng chứa Acute Bacterial Sinusitis sang file mới
+    filtered_file_path = 'Acute_Bacterial_Sinusifalkfhskhfakfhjsflshkfdjdhlkajhfdlsdkjhfkjahlkf.xlsx'          #bạn muốn đổi tên ntn cũng được :))
+    filtered_rows.to_excel(filtered_file_path, index=False)             #các thông tin ko liên quan sẽ ko được đổi tên như trên
+    # lưu lại các thông tin ko liên quan sang file mới 
+    updated_file_path = 'hkfshaflksjhfkjahlsfhskfa.xlsx'         # đặt tên ntn cũng được
+    remaining_data.to_excel(updated_file_path, index=False)
+    print(f"Filtered rows saved to: {filtered_file_path}")       #trong phần mô tả của code, nó sẽ nói lại các thông tin được lưu ở file nào (có hay ko cũng được)
+    print(f"Updated data saved to: {updated_file_path}")
 def Xoa_du_lieu() :
     import pandas as pd
     from openpyxl import load_workbook
@@ -42,7 +61,7 @@ def Xoa_du_lieu() :
     print(df)
     # Save the excel file - phần đặt tên file , đặt gì cũng được
     df.to_excel("drug sus.xlsx", index=False)
-def Bieu_do() :
+def Bieu_do_tron() :
     # Khai báo thu viện
     import pandas as pd 
     import matplotlib.pyplot as plt
@@ -56,15 +75,49 @@ def Bieu_do() :
     ax.pie(a,explode=explode,labels=b,autopct='%1.2f%%') # a:giá trị của từng pie , explode: chỉ ra khoảng cách của từng slice , gán nhãn từ cột effective cho mỗi pie , autopct : hiển thị phần trăm trong tuừng pie với 2 chữ số thập phân 
     # Hiển thị biểu đồ
     plt.show()
+def Bieu_do_cot() :
+    # Khai báo thư viện
+    import pandas as pd
+    import matplotlib.pyplot as plt
+    # Đọc file Excel
+    data=pd.read_excel("d:/Drug (1).xlsx",engine="openpyxl")
+    # Lấy cột dữ liệu Condition và chuyển thành kiểu danh sách với điều kiện chỉ lấy 1 dữ liệu duy nhất nếu bị trùng 
+    conditions=data['Condition'].unique().tolist()
+    # Tạo danh sách dùng để chứa số lượng thuốc của mỗi loại bệnh
+    lst_drug=[]
+    # Duyệt qua từng dữ liệu của danh sách Conditions ( Đi qua từng loại bệnh trong danh sách)
+    for condition in conditions:
+        # Lọc dữ liệu lấy những hàng với điều kiện giống nhau về tên bệnh
+        data_new=data.loc[data['Condition']==condition] 
+        # Lấy cột dữ liệu Drug trong data vừa lọc ở trên và chuyển thành kiểu danh sách với điều kiện chỉ lấy 1 dữ liệu duy nhất nếu bị trùng 
+        drugs=data_new['Drug'].unique().tolist() 
+        # Đếm số lượng thuốc dùng để chữa bệnh
+        count_drug=len(drugs) 
+        # Thêm tổng số thuốc dùng để chữa trị bệnh vào danh sách
+        lst_drug.append(count_drug)
+    # In ra danh sách cột Conditions và danh sách số lượng thuốc
+    print(conditions)
+    print(lst_drug)
+    # Vẽ biểu đồ cột với trục ngang là tên các bệnh và trục dọc là số lượng thuốc điều trị bệnh đó
+    plt.bar(conditions,lst_drug,color='green') 
+    plt.xticks(rotation=30,ha="right") # Xoay tên của các cột 45 độ theo chiều kim đồng hồ và căn chỉnh các nhãn về phía bên phải 
+    plt.yticks(range(0,100,5)) # Hiển thị giá trị cột y từ 0 đến 95 và mỗi đơn vị liên tiếp cách nhau 5 
+    plt.xlabel('Triệu chứng bệnh') # Đặt tên trục x
+    plt.ylabel('Số lượng thuốc') # đặt tên trục y
+    plt.title('Tổng số lượng thuốc điều trị của mỗi bệnh') # Đặt tên biểu đồ
+    plt.subplots_adjust(bottom=0.26) # Dịch chuyển khung biểu đồ lên trên
+    plt.show() # Hiển thị biểu đồ
 def Khong_hop_le() :
     print("Lựa chọn không hợp lệ!")
 luachon_dict = { 
     1: Doc_du_lieu ,
     2: Cap_nhat_du_lieu ,
     3: Tao_du_lieu_moi ,
-    4: Xoa_du_lieu ,
-    5: Bieu_do ,
-    6: Khong_hop_le
+    4: Tao_data ,
+    5: Xoa_du_lieu ,
+    6: Bieu_do_tron ,
+    7: Bieu_do_cot ,
+    8: Khong_hop_le
 }
 def menu() :
     while True :
@@ -73,9 +126,11 @@ def menu() :
         print("1.Doc_du_lieu")
         print("2.Cap_nhat_du_lieu")
         print("3.Tao_du_lieu_moi")
-        print("4.Xoa_du_lieu")
-        print("5.Bieu_do")
-        print("6.Thoat_chuong_trinh")
+        print("4.Tao_data")
+        print("5.Xoa_du_lieu")
+        print("6.Bieu_do_tron")
+        print("7.Bieu_do_cot")
+        print("8.Thoat_chuong_trinh")
         chon = input("Mời chọn tính năng:")
         if chon.isdigit() :
             chon = int(chon)
@@ -87,3 +142,4 @@ def menu() :
             print("Hãy nhập lại,dữ liệu là Số nhé !")
 # Chạy menu
 menu()
+       
